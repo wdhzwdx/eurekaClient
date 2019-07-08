2e971def8512fd734d265b4c1da733c7e38bcfe7
wdhzwdx <757805061@qq.com>
2019-07-08 14:53:05 +0800

    Update EurekaClientApplication.java

--- src/main/java/com/example/demo/EurekaClientApplication.java
+++ src/main/java/com/example/demo/EurekaClientApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class EurekaClientApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientApplication.class, args);
 	}
